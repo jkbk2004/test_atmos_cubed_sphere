c66f2303b49cfeb69d4414fc9ec7dd983b2ff169
jkbk2004 <jong.kim@noaa.gov>
2023-08-24 12:51:26 -0500

    more clean up old fmsio

--- model/molecular_diffusion.F90
+++ model/molecular_diffusion.F90
@@ -41,7 +41,7 @@ module molecular_diffusion_mod
       use constants_mod,      only: rdgas, cp_air
       use fv_mp_mod,          only: is_master
       use mpp_mod,            only: stdlog, input_nml_file
-      use fms_mod,            only: check_nml_error, open_namelist_file, close_file
+      use fms_mod,            only: check_nml_error
       use fv_grid_utils_mod,  only: g_sum
       use mpp_domains_mod,    only: domain2d
       use fv_arrays_mod,      only: fv_grid_type, fv_grid_bounds_type
